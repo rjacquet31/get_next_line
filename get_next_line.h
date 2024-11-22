72cf4db8e8619d2f896570a35438f564d651ec1c
deluxeee31 <rafael.jacquet.31@gmail.com>
2024-11-22 19:12:21 +0100

    files

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 18:06:39 by rafael            #+#    #+#             */
+/*   Updated: 2024/11/22 18:06:39 by rafael           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+char *get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+
+#endif
