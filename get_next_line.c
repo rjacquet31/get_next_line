78c0eabfbb45b65eea940d5d12741a1d83c521c9
deluxeee31 <rafael.jacquet.31@gmail.com>
2024-12-16 13:57:43 +0100

    finish

--- get_next_line.c
+++ get_next_line.c
@@ -10,12 +10,72 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static char	*text_to_stash(int fd, char *buf, char *stash)
+{
+	int		read_line;
+	char	*temp;
+
+	read_line = 1;
+	while (read_line > 0)
+	{
+		read_line = read(fd, buf, BUFFER_SIZE);
+		if (read_line == -1)
+			return (NULL);
+		if (read_line == 0)
+			break ;
+		buf[read_line] = '\0';
+		if (!stash)
+			stash = ft_strdup("");
+		temp = stash;
+		stash = ft_strjoin(temp, buf);
+		free(temp);
+		if (!stash)
+			return (NULL);
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	return (stash);
+}
+
+static char	*extract(char *line)
+{
+	size_t	count;
+	char	*stash;
+
+	count = 0;
+	while (line[count] != '\n' && line[count] != '\0')
+		count++;
+	if (line[count] == '\0')
+		return (NULL);
+	stash = ft_substr(line, count + 1, ft_strlen(line) - count - 1);
+	if (!stash)
+		return (NULL);
+	if (*stash == '\0')
+	{
+		free(stash);
+		stash = NULL;
+	}
+	line[count + 1] = '\0';
+	return (stash);
+}
+
 char	*get_next_line(int fd)
 {
-	char	line;
+	char		*line;
+	char		*buf;
+	static char	*stash;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
-	line = get_current_line(fd);
+		return (NULL);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	line = text_to_stash(fd, buf, stash);
+	free(buf);
+	if (!line)
+		return (NULL);
+	stash = extract(line);
 	return (line);
 }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,31 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/22 18:06:39 by rafael            #+#    #+#             */
-/*   Updated: 2024/11/22 18:06:39 by rafael           ###   ########.fr       */
+/*   Created: 2024/12/13 09:10:24 by rafael            #+#    #+#             */
+/*   Updated: 2024/12/13 09:10:24 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# include <stdio.h>
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <limits.h>
 # include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
 # include <unistd.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
 # endif
 
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-int		ft_strlen(const char *str);
-char	*ft_strchr(const char *s, int i);
-char	*ft_strdup(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
 char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int searchedChar);
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elementCount, size_t elementSize);
+
+size_t	ft_strlen(const char *theString);
 
 #endif
